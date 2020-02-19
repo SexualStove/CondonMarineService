1a02882f9111b0c808bf06b69bfabac3325aebc1
Rob Condon <rtc33@uclive.ac.nz>
2020-02-20 03:46:04 +1300

    Does this fix it?

--- src/components/Home/HomePageSplashScreen.vue
+++ src/components/Home/HomePageSplashScreen.vue
@@ -129,13 +129,13 @@
       transform: translate3d(0, -5vw, 0) rotate(0deg);
     }
     25% {
-      transform: rotate(-3deg);
+      transform: translate3d(0, -2vw, 0)  rotate(-3deg);
     }
     50% {
       transform: translate3d(0, 1vw, 0) rotate(0deg);
     }
     75% {
-      transform: rotate(-3deg);
+      transform: translate3d(0, -2vw, 0)  rotate(3deg);
     }
   }
 
