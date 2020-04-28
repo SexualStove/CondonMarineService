8f5b5bd01e3192818f902beae278e3c56630def7
Rob Condon <rtc33@uclive.ac.nz>
2020-04-28 22:13:53 +1200

    Seeing if i added preloadwebpack to fix 1.83s

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,14 @@
 const CompressionPlugin = require('compression-webpack-plugin');
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
 
 module.exports = {
     runtimeCompiler: true,
     chainWebpack(config) {
         config.plugins.delete('prefetch');
         config.plugin('CompressionPlugin').use(CompressionPlugin);
-    }
+        new PreloadWebpackPlugin()
+    },
+
 
 };
 
