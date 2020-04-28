38e02a5e3241902cd2d1f2820549c5fd29850ab2
Rob Condon <rtc33@uclive.ac.nz>
2020-04-28 23:15:03 +1200

    reverting back to no htmlwebpackplugin

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,11 @@
 const CompressionPlugin = require('compression-webpack-plugin');
 const PreloadWebpackPlugin = require('preload-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+//const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     runtimeCompiler: true,
     chainWebpack(config) {
         config.plugins.delete('prefetch');
         config.plugin('CompressionPlugin').use(CompressionPlugin);
-        new HtmlWebpackPlugin();
         new PreloadWebpackPlugin(({
             rel: 'preload',
             include: 'all' // or 'initial'
