2bca2f467594582d16132ffd694852483d166dfe
Rob Condon <rtc33@uclive.ac.nz>
2020-04-28 23:00:17 +1200

    Tryna get preload to work

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,24 @@
 const CompressionPlugin = require('compression-webpack-plugin');
 const PreloadWebpackPlugin = require('preload-webpack-plugin');
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
     runtimeCompiler: true,
     chainWebpack(config) {
         config.plugins.delete('prefetch');
         config.plugin('CompressionPlugin').use(CompressionPlugin);
-        new PreloadWebpackPlugin()
+        new HtmlWebpackPlugin();
+        new PreloadWebpackPlugin(({
+            rel: 'preload',
+            include: 'all' // or 'initial'
+        }))
     },
+    configureWebpack: {
+        // It will be merged into the final Webpack config
 
+        plugins: [
+
+        ]
+    }
 
 };
 
