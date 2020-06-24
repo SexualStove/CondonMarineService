bdf3450f24046023e9c43a89b67c7ab9e1c90b92
Rob Condon <rtc33@uclive.ac.nz>
2020-06-25 02:50:40 +1200

    Please jesus if this works ill cry

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default () => {
     return axios.create({
         //baseURL: `https://inhousewebtest.site`
-        baseURL: `https://www.bepaid.co.nz`
+        baseURL: `https://www.bepaid.co.nz:8081`
 
     })
 }
