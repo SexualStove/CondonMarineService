d8c7b3ee45e52054059c76fb56569759fe776165
Rob Condon <rtc33@uclive.ac.nz>
2020-06-25 02:14:15 +1200

    Maybe fix?

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default () => {
     return axios.create({
         //baseURL: `https://inhousewebtest.site`
-        baseURL: `https://www.bepaid.co.nz` || `http://157.245.188.181:8081/`
+        baseURL: `https://www.bepaid.co.nz`
 
     })
 }
