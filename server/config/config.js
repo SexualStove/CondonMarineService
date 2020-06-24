635cc98f0f32cb75dcdc03d2583f6ee5a5c9bbe4
Rob Condon <rtc33@uclive.ac.nz>
2020-06-25 04:25:33 +1200

    Helppp

--- server/config/config.js
+++ server/config/config.js
@@ -5,9 +5,10 @@ module.exports ={
         user: process.env.DB_USER || 'MITBlogger',
         password: process.env.DB_PASS || 'password',
         database: process.env.DB_NAME || 'MITBlog',
-        host: 'https://www.bepaid.co.nz' || 'localhost' || process.env.HOST,
+        host: 'https://www.bepaid.co.nz' || 'localhost' || process.env.HOST || 'http://www.bepaid.co.nz',
         dialect: 'sqlite',
         storage: './MITBlog.sqlite'
     }
+
 
 };
