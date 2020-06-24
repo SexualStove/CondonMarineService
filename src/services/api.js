673c188a68411788c345df124842c015402bb503
Rob Condon <rtc33@uclive.ac.nz>
2020-06-25 03:01:44 +1200

    i wanna fucking die

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 export default () => {
     return axios.create({
         //baseURL: `https://inhousewebtest.site`
-        baseURL: `https://www.bepaid.co.nz/:8081`
+        baseURL: `https://www.bepaid.co.nz:8081`
 
     })
 }
